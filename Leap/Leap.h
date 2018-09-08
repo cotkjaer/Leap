db6c9a31927d4ee3799e38250736612fd52ff75c
Christian Otkjær <christian@otkjaer.dk>
2018-09-08 10:36:59 +0200

    Initial Commit

---
+++ Leap/Leap.h
@@ -0,0 +1,19 @@
+//
+//  Leap.h
+//  Leap
+//
+//  Created by Christian Otkjær on 08/09/2018.
+//  Copyright © 2018 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Leap.
+FOUNDATION_EXPORT double LeapVersionNumber;
+
+//! Project version string for Leap.
+FOUNDATION_EXPORT const unsigned char LeapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leap/PublicHeader.h>
+
+
